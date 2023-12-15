520ace3b18eeef8114389678bccb5fae20000455
t1mmy89ru <56981409+t1mmy89ru@users.noreply.github.com>
2023-12-15 18:49:56 -0500

    Update

--- tests/test_cscqmsspkg.py
+++ tests/test_cscqmsspkg.py
@@ -5,11 +5,25 @@ from get_college_info import get_college_info
 
 def test_get_college_info():
     college_name_input = "Michigan State University"
+    api_key = "api_key"
     result = get_college_info(college_name_input, api_key)
     assert result is not None
     assert result["Name"] == college_name_input
 
-    # Test case for invalid college name
     invalid_college_name = "Invalid College Name"
     result_invalid = get_college_info(invalid_college_name, dummy_api_key)
     assert result_invalid is None
+
+def test_invalid_api_key():
+    college_name_input = "Rutgers University-New Brunswick"
+    invalid_api_key = "invalid_api_key"
+
+    result = get_college_info(college_name_input, invalid_api_key)
+    assert result is None
+
+def test_multiple_college_matches():
+    college_name_input = "State"
+    api_key = "api_key"
+
+    result = get_college_info(college_name_input, api_key)
+    assert result is print(f"Multiple possible matches found for '{college_name}'. Please specify one below and reinput into the function:")
